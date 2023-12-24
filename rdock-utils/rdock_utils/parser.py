4d0ceb6069c821a67d999f06c6443e07762d01c8
ggutierrez-sunbright <ggutierrez@sunbright.tech>
2023-12-24 00:33:23 +0000

    refactor parser

--- rdock-utils/rdock_utils/parser.py
+++ rdock-utils/rdock_utils/parser.py
@@ -1,11 +1,12 @@
+# Standard Library
 import logging
+from io import StringIO
 from typing import Any, TextIO
 
 logger = logging.getLogger("SDParser")
 
 
 class FastSDMol:
-
     def __init__(self, lines: list[str], data: dict[str, str]) -> None:
         self.lines = lines
         self.data = data
@@ -35,9 +36,7 @@ class FastSDMol:
             data[field_name] = field_value.strip("\n")
             discard_line = source.readline()
             if discard_line.startswith("$$$$"):
-                logger.warning(
-                    f"found end of molecule {lines[0]} while expecting empty line after field {field_name}"
-                )
+                logger.warning(f"found end of molecule {lines[0]} while expecting empty line after field {field_name}")
                 break
 
         return cls(lines, data) if len(lines) >= 4 else None
@@ -50,16 +49,18 @@ class FastSDMol:
 
     @staticmethod
     def str_field(field_name: str, field_value: Any) -> str:
-        return f">  <{field_name}>\n{field_value}\n"
+        return f">  <{field_name}>\n{field_value}\n\n"
 
     def __repr__(self) -> str:
-        value = "\n".join([
-            "".join(self.lines),
-            "\n".join(self.str_field(field_name, field_value) for field_name, field_value in self.data.items()),
-            "$$$$",
-            ""
-        ])
-        return value
+        str_io = StringIO()
+        self.write(str_io)
+        return str_io.getvalue()
 
     def __str__(self) -> str:
         return f"<Molecule {self.lines[0]}>"
+
+    def write(self, dest: TextIO) -> None:
+        dest.writelines(self.lines)
+        for field_name, field_value in self.data.items():
+            dest.write(self.str_field(field_name, field_value))
+        dest.write("$$$$")
