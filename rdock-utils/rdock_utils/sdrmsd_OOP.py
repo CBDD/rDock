e78cae75a57e9aa0715e76e8faf2ef4be7090e26
lpardey <lpardey@outlook.com>
2024-01-17 20:44:23 +0000

    replace print sys.stderr for logger implementation on: * main * print_matching_info refactor functions: * calculate_rmsd * handle_pose_matching * process_and_save_selected_molecules remove get automorphism rmsd

--- rdock-utils/rdock_utils/sdrmsd_OOP.py
+++ rdock-utils/rdock_utils/sdrmsd_OOP.py
@@ -1,14 +1,12 @@
 import argparse
 import logging
-import math
-import sys
 
 import numpy
 from openbabel import pybel
 
 from rdock_utils.helper import Helper, HelperData, PoseMatchData
 
-logger = logging.getLogger("sdrmsd")
+logger = logging.getLogger("SDRMSD")
 
 Coordinate = tuple[float, float, float]
 SingularValueDecomposition = tuple[numpy.ndarray[float], numpy.ndarray[float], numpy.ndarray[float]]
@@ -20,10 +18,6 @@ def main(argv: list[str] | None = None) -> None:
     args = parser.parse_args(argv)
     helper = Helper(args.reference, args.input, args.fit, args.threshold, args.out)
 
-    # Read crystal pose
-    crystal_pose = get_crystal_pose(helper.reference_sdf)
-    crystal_atoms = len(crystal_pose.atoms)
-
     # Find the RMSD between the crystal pose and each docked pose
     docked_poses = pybel.readfile("sdf", helper.input_sdf)
 
@@ -31,6 +25,9 @@ def main(argv: list[str] | None = None) -> None:
 
     data = HelperData()
 
+    # Read crystal pose
+    crystal_pose = get_crystal_pose(helper.reference_sdf)
+    crystal_atoms = len(crystal_pose.atoms)
     for i, docked_pose in enumerate(docked_poses, start=1):
         atoms_number = process_docked_pose(docked_pose)
 
@@ -47,7 +44,7 @@ def main(argv: list[str] | None = None) -> None:
         process_and_save_selected_molecules(output_sdf, data)
 
     if data.skipped:
-        print(f"SKIPPED input molecules due to the number of atom mismatch: {data.skipped}", file=sys.stderr)
+        logger.warning(f"SKIPPED input molecules due to the number of atom mismatch: {data.skipped}")
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -129,10 +126,10 @@ def calculate_rmsd(crystal: pybel.Molecule, docked_pose: pybel.Molecule, helper:
     Perform RMSD calculations and update coordinates if required.
     """
     if helper.fit:
-        rmsd_result, fitted_result = get_automorphism_rmsd(crystal, docked_pose, helper)
+        rmsd_result, fitted_result = helper.get_automorphism_rmsd(crystal, docked_pose, helper)
         helper.update_coordinates(docked_pose, fitted_result)
     else:
-        rmsd_result = get_automorphism_rmsd(crystal, docked_pose, helper)
+        rmsd_result = helper.get_automorphism_rmsd(crystal, docked_pose, helper)
 
     return rmsd_result
 
@@ -145,49 +142,31 @@ def handle_pose_matching(rmsd_result: RMSDResult, pose_match_data: PoseMatchData
     out = helper.out
 
     if threshold:
-        match_pose, best_match_value = get_best_matching_pose(pose_match_data, threshold)
+        match_pose, best_match_value = helper.get_best_matching_pose(pose_match_data, threshold)
         if match_pose is not None:
             print_matching_info(pose_match_data, match_pose, best_match_value)
         else:
-            save_or_print_info(rmsd_result, pose_match_data, out)
+            save_and_print_info(rmsd_result, pose_match_data, out)
     else:
-        save_or_print_info(rmsd_result, pose_match_data, out)
+        save_and_print_info(rmsd_result, pose_match_data, out)
 
 
 def process_and_save_selected_molecules(output_sdf: pybel.Outputfile, data: HelperData):
     for i in sorted(data.out_dict.keys()):
         molecule, rmsd_result = data.out_dict[i]
         # Get the number of matches in the thresholding operation
-        population_value = data.population.get(i) or 1
+        population_value = data.population.get(i, 1)
         save_molecule_with_rmsd(output_sdf, molecule, rmsd_result, population_value)
 
 
-# TODO: Review names and best match rmsd
-def get_best_matching_pose(pose_match_data: PoseMatchData, threshold: float) -> tuple[float | None, float]:
-    docked_pose = pose_match_data.docked_pose
-    molecules_dict = pose_match_data.helper_data.molecules_dict
-    match_pose = None
-    best_match_value = 999999.0
-
-    for did, prevmol in molecules_dict.items():
-        tmprmsd = get_automorphism_rmsd(prevmol, docked_pose)
-
-        if tmprmsd < threshold and tmprmsd < best_match_value:
-            best_match_value = tmprmsd
-            match_pose = did
-
-    return (match_pose, best_match_value)
-
-
-def print_matching_info(pose_match_data: PoseMatchData, match_pose: float, best_match_value: float) -> None:
+def print_matching_info(pose_match_data: PoseMatchData, match_pose: int, best_match_value: float) -> None:
     index = pose_match_data.pose_index
     population = pose_match_data.helper_data.population
-
-    print(f"Pose {index} matches pose {match_pose} with {best_match_value:.3f} RMSD", file=sys.stderr)
+    logger.info(f"Pose {index} matches pose {match_pose} with {best_match_value:.3f} RMSD")
     population[match_pose] += 1
 
 
-def save_or_print_info(rmsd_result: RMSDResult, pose_match_data: PoseMatchData, out: bool) -> None:
+def save_and_print_info(rmsd_result: RMSDResult, pose_match_data: PoseMatchData, out: bool) -> None:
     """
     Function to save and print information based on 'out' parser argument.
     """
@@ -204,61 +183,15 @@ def save_or_print_info(rmsd_result: RMSDResult, pose_match_data: PoseMatchData, 
     population[index] = 1
 
 
-def get_automorphism_rmsd(target: pybel.Molecule, molecule: pybel.Molecule, helper: Helper) -> RMSDResult:
-    """
-    Use Automorphism to reorder target coordinates to match reference coordinates atom order
-    for correct RMSD comparison. Only the lowest RMSD will be returned.
-
-    Returns:
-      If fit=False: bestRMSD	(float)					RMSD of the best matching mapping.
-      If fit=True:  (bestRMSD, molecCoordinates)	(float, npy.array)	RMSD of best match and its molecule fitted coordinates.
-    """
-    fit = helper.fit
-    mappings = pybel.ob.vvpairUIntUInt()
-    bit_vector = pybel.ob.OBBitVec()
-    lookup = [i for i, _ in enumerate(target)]
-    success = pybel.ob.FindAutomorphisms(target.OBMol, mappings)
-    target_coordinates = [atom.coords for atom in target]  # TODO: ADAPTED TO ORIGINAL
-    # index_to_target_coordinates = dict(enumerate(target_coordinates))
-    mappose = numpy.array(helper.map_to_crystal(target, molecule))
-    sorted_indices = numpy.argsort(mappose[:, 0])
-    mappose_result = mappose[sorted_indices][:, 1]
-    molecule_coordinates = [atom.coords for atom in molecule]
-    pose_coordinates = numpy.array(molecule_coordinates)[mappose_result]
-    rmsd_result = 999999999999  # TODO: ADAPTED TO ORIGINAL
-
-    # Loop through automorphisms
-    for mapping in mappings:
-        automorph_coords = [None] * len(target_coordinates)  # TODO: ADAPTED TO ORIGINAL
-        for x, y in mapping:  # TODO: ADAPTED TO ORIGINAL
-            automorph_coords[lookup.index(x)] = target_coordinates[lookup.index(y)]  # TODO: ADAPTED TO ORIGINAL
-
-        mapping_rmsd = helper.rmsd(pose_coordinates, automorph_coords)
-
-        # Update result if the current mapping has a lower RMSD
-        if mapping_rmsd < rmsd_result:
-            rmsd_result = mapping_rmsd
-
-        # Additional fitting if fit=True
-        if fit:
-            fitted_pose, fitted_rmsd = helper.superpose_3D(
-                numpy.array(automorph_coords), numpy.array(pose_coordinates)
-            )
-
-            # Update result if the fitted RMSD is lower
-            if fitted_rmsd < rmsd_result:
-                rmsd_result = fitted_rmsd
-
-    return (rmsd_result, fitted_pose) if fit else rmsd_result
-
-
 def save_molecule_with_rmsd(
     output_sdf: pybel.Outputfile, molecule: pybel.Molecule, rmsd: float, population_value: int
 ) -> None:
     new_data = pybel.ob.OBPairData()
     new_data.SetAttribute("RMSD")
     new_data.SetValue(f"{rmsd:.3f}")
-
+    # pasar al helper
+    # gestionar que el population sea opcional no booleano
+    # if threshold
     pop_data = pybel.ob.OBPairData()
     pop_data.SetAttribute("Population")
     pop_data.SetValue(f"{population_value}")
