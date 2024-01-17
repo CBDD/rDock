e287b2f8c6e867fc4a25df3ebebee20aaaac7a39
lpardey <lpardey@outlook.com>
2024-01-17 20:35:27 +0000

    add aligned_nofit_filename and aligned_fit_filename fixtures

--- rdock-utils/tests/sdrmsd/conftest.py
+++ rdock-utils/tests/sdrmsd/conftest.py
@@ -5,16 +5,25 @@ from ..conftest import FIXTURES_FOLDER
 SDRMSD_FIXTURES_FOLDER = FIXTURES_FOLDER / "sdrmsd"
 REF_FILE = str(SDRMSD_FIXTURES_FOLDER / "ref.sdf")
 INPUT_FILE = str(SDRMSD_FIXTURES_FOLDER / "input.sdf")
-ALIGNED_FILE = str(SDRMSD_FIXTURES_FOLDER / "aligned-nofit.sdf")
-ALIGNED_FIT_FILE = str(SDRMSD_FIXTURES_FOLDER / "aligned-fit.sdf")
 
 
 @pytest.fixture(scope="session")
-def aligned_molecules_raw_data() -> bytes:
-    with open(ALIGNED_FILE, "rb") as f:
+def aligned_nofit_filename() -> str:
+    return str(SDRMSD_FIXTURES_FOLDER / "aligned-nofit.sdf")
+
+
+@pytest.fixture(scope="session")
+def aligned_fit_filename() -> str:
+    return str(SDRMSD_FIXTURES_FOLDER / "aligned-fit.sdf")
+
+
+@pytest.fixture(scope="session")
+def aligned_molecules_raw_data(aligned_nofit_filename: str) -> bytes:
+    with open(aligned_nofit_filename, "rb") as f:
         return f.read()
 
+
 @pytest.fixture(scope="session")
-def aligned_fit_molecules_raw_data() -> bytes:
-    with open(ALIGNED_FIT_FILE, "rb") as f:
+def aligned_fit_molecules_raw_data(aligned_fit_filename: str) -> bytes:
+    with open(aligned_fit_filename, "rb") as f:
         return f.read()
