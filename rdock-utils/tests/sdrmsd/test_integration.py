5f8d883b423e159ca470168612140af147983580
lpardey <lpardey@outlook.com>
2024-01-17 20:36:40 +0000

    fix tests: * test_no_fit_resulsts * test_fit_resulsts add functions: * atoms_are_equal * molecules_are_equal * compare_sdf_files

--- rdock-utils/tests/sdrmsd/test_integration.py
+++ rdock-utils/tests/sdrmsd/test_integration.py
@@ -1,6 +1,7 @@
 from tempfile import NamedTemporaryFile
 
 import pytest
+from openbabel import pybel
 
 from rdock_utils.sdrmsd_OOP import main as sdrmsd_main
 from rdock_utils.sdrmsd_original import main as sdrmsd3_main
@@ -28,20 +29,49 @@ def test_basic_run(main):
 
 
 @parametrize_main
-def test_no_fit_resulsts(main, aligned_molecules_raw_data: bytes):
+def test_no_fit_resulsts(main, aligned_nofit_filename: str, capsys: pytest.CaptureFixture[str]):
     with NamedTemporaryFile() as tmp:
         args = ["-o", tmp.name, REF_FILE, INPUT_FILE]
         main(args)
-        with open(tmp.name, "rb") as f:
-            output = f.read()
-        assert output == aligned_molecules_raw_data
+        assert compare_sdf_files(tmp.name, aligned_nofit_filename)
+        assert capsys.readouterr().out == "POSE\tRMSD_NOFIT\n1\t0.00\n2	6.69\n3	0.48\n4	0.12\n5	0.14\n"
 
 
 @parametrize_main
-def test_fit_resulsts(main, aligned_fit_molecules_raw_data: bytes):
+def test_fit_resulsts(main, aligned_fit_filename: str):
     with NamedTemporaryFile() as tmp:
         args = ["-f", "-o", tmp.name, REF_FILE, INPUT_FILE]
         main(args)
-        with open(tmp.name, "rb") as f:
-            output = f.read()
-        assert output == aligned_fit_molecules_raw_data
+        assert compare_sdf_files(tmp.name, aligned_fit_filename)
+
+
+def atoms_are_equal(atom_1: pybel.Atom, atom_2: pybel.Atom) -> bool:
+    if atom_1.atomicnum != atom_2.atomicnum:
+        return False
+
+    if atom_1.coords != atom_2.coords:
+        return False
+
+    if atom_1.formalcharge != atom_2.formalcharge:
+        return False
+
+    if atom_1.type != atom_2.type:
+        return False
+
+    return True
+
+
+def molecules_are_equal(molecule_1: pybel.Molecule, molecule_2: pybel.Molecule) -> bool:
+    molecule_1_data = {k: v for k, v in molecule_1.data.items()}
+    molecule_2_data = {k: v for k, v in molecule_2.data.items()}
+
+    if molecule_1_data != molecule_2_data:
+        return False
+
+    return all(atoms_are_equal(atom_1, atom_2) for atom_1, atom_2 in zip(molecule_1.atoms, molecule_2.atoms))
+
+
+def compare_sdf_files(filename_1: str, filename_2: str) -> bool:
+    molecules_1 = pybel.readfile("sdf", filename_1)
+    molecules_2 = pybel.readfile("sdf", filename_2)
+    return all(molecules_are_equal(molecule_1, molecule_2) for molecule_1, molecule_2 in zip(molecules_1, molecules_2))
