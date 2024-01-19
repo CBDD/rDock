45de3d7f5ad8fbe0466dc59e7296449d0606bfdf
lpardey <lpardey@outlook.com>
2024-01-19 20:09:08 +0000

    add parser file

---
+++ rdock-utils/rdock_utils/sdrmds/parser.py
@@ -0,0 +1,53 @@
+import argparse
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="SDRMSD",
+        usage="%(prog)s [options] reference.sdf input.sdf",
+        description="Superpose molecules before RMSD calculation",
+        epilog=(
+            "Arguments:\n"
+            "   reference.sdf   SDF file with the reference molecule.\n"
+            "   input.sdf       SDF file with the molecules to be compared to reference.\n"
+        ),
+    )
+    parser.add_argument(
+        "reference",
+        type=str,
+        help="Path to the SDF file with the reference molecule.",
+    )
+    parser.add_argument(
+        "input",
+        type=str,
+        help="Path to the SDF file with the molecules to be compared to reference.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fit",
+        action="store_true",
+        default=False,
+        help="Superpose molecules before RMSD calculation",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        action="store",
+        type=float,
+        help=(
+            "Discard poses with RMSD < THRESHOLD with respect previous poses "
+            "which were not rejected based on the same principle. A Population "
+            "SDField will be added to output SD with the population number."
+        ),
+    )
+    parser.add_argument(
+        "-o",
+        "--out",
+        default=False,
+        metavar="FILE",
+        help=(
+            "If declared, write an output SDF file with the input molecules with "
+            "a new sdfield <RMSD>. If the molecule was fitted, the fitted molecule coordinates will be saved."
+        ),
+    )
+    return parser
