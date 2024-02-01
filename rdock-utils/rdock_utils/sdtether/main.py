e42a2729d8703865fc19b4f43ff99ee8f6a4ef0f
lpardey <lpardey@outlook.com>
2024-02-01 21:29:56 +0000

    add sdtether main function

---
+++ rdock-utils/rdock_utils/sdtether/main.py
@@ -0,0 +1,12 @@
+from rdock_utils.sdtether.parser import get_config
+from rdock_utils.sdtether.sdtether import SDTether
+
+
+def main(argv: list[str] | None = None):
+    config = get_config(argv)
+    sdtether = SDTether(config)
+    sdtether.run()
+
+
+if __name__ == "__main__":
+    main()
