a3a912438f7e8ac86f464e34f8f2b5cbe0b4895d
ggutierrez-sb <ggutierrez@sunbright.tech>
2024-12-14 00:23:39 +0000

    add modifications to bin_read/write string specialization for retrocompatibility

--- include/RbtBinaryIO.h
+++ include/RbtBinaryIO.h
@@ -48,16 +48,23 @@ inline void bin_read(std::istream& istr, T* data, size_t n) {
 
 template <>
 inline void bin_write(std::ostream& ostr, const std::string& data) {
-    bin_write(ostr, data.size());
-    bin_write(ostr, data.data(), data.size());
+    // casting size_t to int, and storing the null character at the end
+    // for retro-compatibility with old code only. This is not the best way to do it
+    // and will be modified in the near future
+    bin_write(ostr, (int)(data.size() + 1));
+    bin_write(ostr, data.data(), data.size() + 1);
 }
 
 template <>
 inline void bin_read(std::istream& istr, std::string& data) {
-    size_t size;
+    // similar to the bin_write(std::ostream& ostr, const std::string& data) function
+    // this is a temporary solution for retro-compatibility with old code
+    // the size_t is casted to int, and the null character added at the end must be removed
+    int size;
     bin_read(istr, size);
     data.resize(size);
-    bin_read(istr, &data[0], size);
+    bin_read(istr, data.data(), size);
+    data.pop_back();
 }
 
 }  // namespace Rbt
