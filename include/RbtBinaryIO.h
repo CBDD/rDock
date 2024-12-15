b35fd59d7d9044bc2b0229b287661ba605848b90
ggutierrez-sb <ggutierrez@sunbright.tech>
2024-12-15 03:29:09 +0000

    homogenize string serialization behavior with old code

--- include/RbtBinaryIO.h
+++ include/RbtBinaryIO.h
@@ -48,23 +48,21 @@ inline void bin_read(std::istream& istr, T* data, size_t n) {
 
 template <>
 inline void bin_write(std::ostream& ostr, const std::string& data) {
-    // casting size_t to int, and storing the null character at the end
-    // for retro-compatibility with old code only. This is not the best way to do it
-    // and will be modified in the near future
-    bin_write(ostr, (int)(data.size() + 1));
-    bin_write(ostr, data.data(), data.size() + 1);
+    // casting size_t to int for retro-compatibility with old code only.
+    // This is not the best way to do it and will be modified in the near future
+    bin_write(ostr, (int)(data.size()));
+    bin_write(ostr, data.data(), data.size());
 }
 
 template <>
 inline void bin_read(std::istream& istr, std::string& data) {
     // similar to the bin_write(std::ostream& ostr, const std::string& data) function
     // this is a temporary solution for retro-compatibility with old code
-    // the size_t is casted to int, and the null character added at the end must be removed
+    // the size_t is casted to int
     int size;
     bin_read(istr, size);
     data.resize(size);
     bin_read(istr, data.data(), size);
-    data.pop_back();
 }
 
 }  // namespace Rbt
